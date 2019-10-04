58fa84f5f00c4ee10e7495183d10d6c973dbfa93
Wojciech Baranowski <wbaranowski@protonmail.com>
2019-10-04 10:28:09 +0300

    Handle multithreading

--- src/libstd/stacktraceflow.rs
+++ src/libstd/stacktraceflow.rs
@@ -3,8 +3,13 @@
 
 use crate::fs::File;
 use crate::io::Write;
+use crate::thread;
+use crate::cell::RefCell;
 
-static mut DUMP_FILE: Option<File> = None;
+thread_local! {
+static DUMP_FILE: RefCell<Option<File>> = RefCell::new(None);
+}
+
 /// Dummy
 #[derive(Debug)]
 #[stable(feature = "stacktraceflow", since = "1.3.0")]
@@ -16,12 +21,14 @@ impl StackTraceFlower {
     /// Dummy
     #[stable(feature = "stacktraceflow", since = "1.3.0")]
     pub fn new(s: &'static str) -> StackTraceFlower {
-        unsafe{
-            if let None = DUMP_FILE {
-                DUMP_FILE = Some(File::create("dump_file.txt").unwrap());
+        DUMP_FILE.with(|f| {
+            let mut f_ref = f.borrow_mut();
+            if let None = *f_ref {
+                let filename = format!("dump_file.{:?}.txt", thread::current().id());
+                *f_ref = Some(File::create(&filename).unwrap());
             }
-            write!(DUMP_FILE.as_mut().unwrap(), "+{}\n", &s).unwrap();
-        }
+            write!(f_ref.as_mut().unwrap(), "+{}\n", &s).unwrap();
+        });
         StackTraceFlower{s}
     }
 }
@@ -29,8 +36,8 @@ impl StackTraceFlower {
 #[stable(feature = "stacktraceflow", since = "1.3.0")]
 impl Drop for StackTraceFlower {
     fn drop(&mut self) {
-        unsafe{
-            write!(DUMP_FILE.as_mut().unwrap(), "-{}\n", &self.s).unwrap();
-        }
+        DUMP_FILE.with(|f| {
+            write!(f.borrow_mut().as_mut().unwrap(), "-{}\n", &self.s).unwrap();
+        });
     }
 }
